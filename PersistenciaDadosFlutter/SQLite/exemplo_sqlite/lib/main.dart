adde3c6f14e13a2d93bfc5c7e7100a4440cfcde8
Vitor-Hott-Prado <vitorhott061@gmail.com>
2025-05-21 15:47:22 -0300

    21.05.2025

--- PersistenciaDadosFlutter/SQLite/exemplo_sqlite/lib/main.dart
+++ PersistenciaDadosFlutter/SQLite/exemplo_sqlite/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:exemplo_sqlite/views/nota.view.dart';
 import 'package:flutter/material.dart';
+
+
 
 void main(){
   runApp(MaterialApp(
-    home: NotasView(),
+    home: NotaView(),
   ));
 }
