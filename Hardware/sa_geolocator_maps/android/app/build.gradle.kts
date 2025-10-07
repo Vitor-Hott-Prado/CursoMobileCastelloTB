66473f4ad0d0e3bfac7283b26943f9bd34952a7c
Vitor-Hott-Prado <vitorhott061@gmail.com>
2025-10-07 11:24:21 -0300

    07.10.2025

--- Hardware/sa_geolocator_maps/android/app/build.gradle.kts
+++ Hardware/sa_geolocator_maps/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.sa_geolocator_maps"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
