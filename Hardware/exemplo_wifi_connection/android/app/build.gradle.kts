e37a6a1bf28f94346b47de54209ee704630cc769
Vitor-Hott-Prado <vitorhott061@gmail.com>
2025-10-07 09:03:33 -0300

    07.09.2025

--- Hardware/exemplo_wifi_connection/android/app/build.gradle.kts
+++ Hardware/exemplo_wifi_connection/android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.exemplo_geolocator"
-    compileSdk = 36
+    namespace = "com.example.exemplo_wifi_connection"
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.exemplo_geolocator"
+        applicationId = "com.example.exemplo_wifi_connection"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
